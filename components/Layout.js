55b209bd7535bd4aae85788d96d5861073f27f4c
Piotr Glodek <pioglodek@gmail.com>
2021-02-01 12:06:49 +0100

    Create Layout component

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+// components
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
