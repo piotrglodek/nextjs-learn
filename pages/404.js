6e6db624620353ca07e50bae40a8ecfbb0a0a050
Piotr Glodek <pioglodek@gmail.com>
2021-02-02 09:51:10 +0100

    Add auto redirecting after time in seconds

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,29 @@
 // next
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+// react
+import { useEffect, useState } from 'react';
 
 export default function NotFound() {
+  const router = useRouter();
+  const [seconds, setSeconds] = useState(10);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(prev => prev - 1);
+      }
+      if (seconds === 0) {
+        // -1 go to prev history page
+        // router.go(-1);
+        router.push('/');
+        clearInterval(myInterval);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
+
   return (
     <div className='not-found'>
       <h1>Ooops...</h1>
@@ -9,6 +31,7 @@ export default function NotFound() {
       <Link href='/'>
         <a>Go back to home page</a>
       </Link>
+      <p>redirecting to home in {seconds}s</p>
     </div>
   );
 }
