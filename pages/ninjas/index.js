6dceebf4be2bec99201f9d03c0d700d8fe802a74
Piotr Glodek <pioglodek@gmail.com>
2021-02-02 18:17:32 +0100

    Add ninjas list fetching data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,12 +1,33 @@
 // components
 import Head from '../../components/Head';
+// styles
+import styles from '../../styles/Ninjas.module.css';
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const data = await (
+    await fetch(`https://jsonplaceholder.typicode.com/users`)
+  ).json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head title='Ninjas' />
       <div>
         <h1>All ninjas</h1>
+        {ninjas.map(ninja => (
+          <div key={ninja.id}>
+            <a href='' className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
